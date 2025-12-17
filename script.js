a3ec289c5c4d4322d14b9a4b37f53ca17a3e757d
Nassim <167772440+NassimSadik@users.noreply.github.com>
2025-12-17 16:34:33 +0100

    Create computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3);
+  let value = num === 0 ? 'Rock' : num === 1 ? 'Paper' : 'Scissors';
+
+  return value;
+}
+
