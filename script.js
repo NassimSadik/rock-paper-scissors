4fea6f2da10e1713ceaec41c0ed020f57a39221c
Nassim <167772440+NassimSadik@users.noreply.github.com>
2026-01-05 18:01:09 +0100

    Add basic UI

--- script.js
+++ script.js
@@ -5,55 +5,71 @@ function getComputerChoice() {
   return value;
 }
 
-function getHumanChoice() {
-  let value = prompt('Enter your choice (Rock Paper or Scissors) : ');
-  return value;
-}
-
 let humanScore = 0;
 let computerScore = 0;
+const scoreToReach = 5;
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice =
-    humanChoice.charAt(0).toUpperCase() +
-    humanChoice.substring(1).toLowerCase();
+  results.textContent = '';
 
   if (
     (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
     (humanChoice === 'Paper' && computerChoice === 'Rock') ||
     (humanChoice === 'Scissors' && computerChoice === 'Paper')
   ) {
-    console.log('You won! ' + humanChoice + ' beats ' + computerChoice + '.');
-    console.log(
-      `Player score = ${(humanScore += 1)} | Computer score = ${computerScore}`
-    );
+    results.textContent = `You won! ${humanChoice} beats ${computerChoice}.
+    Player score = ${(humanScore += 1)} | Computer score = ${computerScore}`;
   } else if (
     (humanChoice === 'Rock' && computerChoice === 'Paper') ||
     (humanChoice === 'Paper' && computerChoice === 'Scissors') ||
     (humanChoice === 'Scissors' && computerChoice === 'Rock')
   ) {
-    console.log('You lose. ' + computerChoice + ' beats ' + humanChoice + '.');
-    console.log(
-      `Player score = ${humanScore} | Computer score = ${(computerScore += 1)}`
-    );
+    results.textContent = `You lose. ${computerChoice} beats ${humanChoice}.
+    Player score = ${humanScore} | Computer score = ${(computerScore += 1)}`;
   } else {
-    console.log('Its a draw! You and the computer choose ' + humanChoice + '.');
-    console.log(
-      `Player score = ${humanScore} | Computer score = ${computerScore}`
-    );
+    results.textContent = `It's a draw! You and the computer choose ${humanChoice}.
+    Player score = ${humanScore} | Computer score = ${computerScore}`;
+  }
+
+  if (humanScore == scoreToReach) {
+    finalResult.textContent = `You have reached 5 points first! You win!`;
+    results.appendChild(finalResult);
+  } else if (computerScore == scoreToReach) {
+    finalResult.textContent = `Computer have reached 5 points first. You lose.`;
+    results.appendChild(finalResult);
   }
 }
 
-function playGame() {
-  let round = 0;
-  while (round != 5) {
+function gameInProgress() {
+  if (humanScore == scoreToReach || computerScore == scoreToReach) {
+    return false;
+  } else return true;
+}
+
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+const results = document.querySelector('.results');
+const finalResult = document.createElement('div');
+
+rockButton.addEventListener('click', () => {
+  if (gameInProgress()) {
     const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
-
-    playRound(humanChoice, computerChoice);
-    round += 1;
-    console.log(`Rounds remaining : ${5 - round}`);
+    playRound('Rock', computerChoice);
   }
-}
+});
 
-playGame();
+paperButton.addEventListener('click', () => {
+  if (gameInProgress()) {
+    const computerChoice = getComputerChoice();
+    playRound('Paper', computerChoice);
+  }
+});
+
+scissorsButton.addEventListener('click', () => {
+  if (gameInProgress()) {
+    const computerChoice = getComputerChoice();
+    playRound('Scissors', computerChoice);
+  }
+});
